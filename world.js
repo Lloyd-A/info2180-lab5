1aec50a61fdb4494664c0c194b9b35e86e97a30f
lallen <lallen@griddynamics.com>
2023-11-21 00:10:04 -0500

    Final edits for lookup Cities

--- world.js
+++ world.js
@@ -8,9 +8,7 @@ window.onload = function () {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("result").innerHTML = this.responseText;
-
-        // Assuming the response contains 'countries' key
+        var response = document.getElementById("result").innerHTML = this.responseText;
         if (response.countries) {
           updateTable(response.countries);
         }
@@ -21,11 +19,31 @@ window.onload = function () {
     xhttp.send();
   });
 
+    document.getElementById("lookupCity").addEventListener("click", function (event) {
+    event.preventDefault();
+    country = document.getElementById("country").value.trim().replace(/(<([^>]+)>)/gi, "");
+
+    if (country !== "") {
+      const xhttp2 = new XMLHttpRequest();
+      xhttp2.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          var response = document.getElementById("result").innerHTML = this.responseText;
+
+          if (response.cities) {
+            updateCityTable(response.cities);
+          }
+        }
+      };
+
+      xhttp2.open("GET", "world.php?country=" + country + "&lookup=cities");
+      xhttp2.send();
+    }
+  });
 
 
   function updateTable(data) {
     // Assuming 'data' is an array of objects with the same structure
-    const table = "<table><tr><th>Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
+    var table = "<table><tr><th>Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
     data.forEach(function (row) {
       table += "<tr><td>" + row.name + "</td><td>" + row.continent + "</td><td>" + row.independence_year + "</td><td>" + row.head_of_state + "</td></tr>";
     });
@@ -33,4 +51,23 @@ window.onload = function () {
 
     document.getElementById("result").innerHTML = table;
   }
+
+function updateCityTable(data) {
+    var table =
+          "<table><tr><th>Name</th><th>District</th><th>Population</th></tr>";
+        data.forEach(function (row) {
+          table +=
+            "<tr><td>" +
+            row.name +
+            "</td><td>" +
+            row.district +
+            "</td><td>" +
+            row.population +
+            "</td></tr>";
+        });
+        table += "</table>";
+
+        result.innerHTML = table;
+  }
+
 };
--- world.php
+++ world.php
@@ -5,27 +5,56 @@ $password = 'password123';
 $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-if(isset($_GET['country'])){
+if(isset($_GET['country']) && !(isset($_GET['lookup']) && $_GET['lookup'] === 'cities')){
   $searchVal = $_GET['country'];
   $country = filter_var($searchVal, FILTER_SANITIZE_STRING);
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+if(isset($_GET['lookup']) && $_GET['lookup'] === 'cities'){
+  $searchVal = $_GET['country'];
+  $city = filter_var($searchVal, FILTER_SANITIZE_STRING);
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population
+                        FROM cities
+                        JOIN countries
+                        ON cities.country_code = countries.code WHERE countries.name LIKE '%$city%'");
+  $cities = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
-<table>
-  <tr>
-  <th>Name</th>
-  <th>Continent</th>
-  <th>Independence Year</th>
-  <th>Head of State</th>
-  </tr>
-<?php foreach ($results as $row): ?>
-<tr>
-  <td><?= $row['name']; ?></td>
-  <td><?= $row['continent']; ?></td>
-  <td><?= $row['independence_year']; ?></td>
-  <td><?= $row['head_of_state']; ?></td>
-  </tr>
-  <?php endforeach; ?>
-</table>
+<?php if(isset($results)): ?>
+    <table>
+      <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+      </tr>
+    <?php foreach ($results as $row): ?>
+    <tr>
+
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['continent']; ?></td>
+      <td><?= $row['independence_year']; ?></td>
+      <td><?= $row['head_of_state']; ?></td>
+    </tr>
+      <?php endforeach; ?>
+    </table>
+<?php endif; ?>
+
+<?php if(isset($cities)): ?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+    <?php foreach ($cities as $crow): ?>
+      <tr>
+        <td><?= $crow['name']; ?></td>
+        <td><?= $crow['district']; ?></td>
+        <td><?= $crow['population']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endif; ?>
